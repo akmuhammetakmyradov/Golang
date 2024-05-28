a5a1bdb1fe2ccb577c824a15496f842a4b71585f
akmuhammetakmyradow <akmyradowakmuhammet21@gmail.com>
2024-05-28 20:12:23 +0500

    unbuffered channels learned

--- Channels/unbuffered/main.go
+++ Channels/unbuffered/main.go
@@ -1,9 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	// orders := make(chan string) // unbuffered channel
+	orders := make(chan string) // unbuffered channel
 
-	// go func(){
+	go func() {
+		for i := 1; i < 5; i++ {
+			order := fmt.Sprintf("Coffee order #%d", i)
+			orders <- order // Blocks until barista is ready to accept
+			fmt.Println("Placed: ", order)
+		}
+		close(orders)
+	}()
 
-	// }
+	for order := range orders {
+		fmt.Printf("Preparing order: %s\n", order)
+		time.Sleep(2 * time.Second) // Time taken for preparing order
+		fmt.Printf("Coffee served: %s\n", order)
+	}
 }
