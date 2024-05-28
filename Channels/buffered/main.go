050afe0316d265fbe1ecf810722529abdc2cb4fe
akmuhammetakmyradow <akmyradowakmuhammet21@gmail.com>
2024-05-28 23:15:23 +0500

    learned buffered channels

--- Channels/buffered/main.go
+++ Channels/buffered/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	orders := make(chan string, 5)
+
+	go func() {
+		for i := 1; i <= 5; i++ {
+			order := fmt.Sprintf("Coffee order %d", i)
+			orders <- order
+			fmt.Printf("Placed %s\n", order)
+		}
+		close(orders)
+	}()
+
+	for order := range orders {
+		fmt.Printf("Prepearing order %s\n", order)
+		time.Sleep(2 * time.Second) // get time for preparing coffee
+		fmt.Printf("Coffee served %s\n", order)
+	}
+}
